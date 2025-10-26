f20484f8d65f7ff71774fdbe456c957a15320571
Abbas Badreddine <abbas.bdr@hotmail.com>
2025-10-26 22:27:12 +0100

    Revert from create to update

--- src/main/java/dat/config/HibernateConfig.java
+++ src/main/java/dat/config/HibernateConfig.java
@@ -80,7 +80,7 @@ public class HibernateConfig {
     private static Properties setBaseProperties(Properties props) {
         props.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
         props.put("hibernate.connection.driver_class", "org.postgresql.Driver");
-        props.put("hibernate.hbm2ddl.auto", "create");
+        props.put("hibernate.hbm2ddl.auto", "update");
         props.put("hibernate.current_session_context_class", "thread");
         props.put("hibernate.show_sql", "true");
         props.put("hibernate.format_sql", "true");
@@ -112,7 +112,7 @@ public class HibernateConfig {
         props.put("hibernate.connection.password", "postgres");
         props.put("hibernate.archive.autodetection", "class");
         props.put("hibernate.show_sql", "true");
-        props.put("hibernate.hbm2ddl.auto", "create-drop"); // update for production
+        props.put("hibernate.hbm2ddl.auto", "update"); // update for production
         return props;
     }
 }
